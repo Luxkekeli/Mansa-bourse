cbd3d3c03a2b638d39e44e5d354c4a1f163532a3
Luxkekeli <luxkekeli@gmail.com>
2026-05-02 17:59:13 +0200

    Correction finale : index.html a la racine

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
-    rollupOptions: {
-      // Si ton fichier index.html est à la racine, laisse simplement la ligne ci-dessous.
-      // Si ton index.html est dans un dossier "frontend", remplace par 'frontend/index.html'
-      input: 'frontend/app.html' 
-    }
+    target: 'esnext'
   }
 })
